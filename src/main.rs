9b05d2a6a495b6e316502aa3454967533667611d
Maxime Augier <max@xolus.net>
2024-02-07 15:29:31 +0100

    Bump deps, options to choose nameserver

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
 use clap::Parser;
 use trust_dns_resolver::{
-    Resolver,
-    config::{ResolverConfig,ResolverOpts}, Name, IntoName,
+    config::{NameServerConfig, Protocol, ResolverConfig, ResolverOpts}, error::ResolveError, IntoName, Name, Resolver
 };
 use trust_dns_proto::{rr::RecordType::NSEC, error::ProtoError};
-use std::{error::Error, process::ExitCode};
+use std::{error::Error, net::SocketAddr, process::ExitCode};
+use thiserror::Error;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about=None)]
 struct Args {
-    domain: String
+    #[arg(short, long)]
+    nameserver: Option<String>,
+    #[arg(short, long)]
+    udp: bool,
+    domain: String,
 }
 
 struct NSECWalker<'r> {
@@ -18,32 +22,47 @@ struct NSECWalker<'r> {
     current: Name,
 }
 
+#[derive(Debug, Error)]
+#[error("NSec")]
+enum NSecError {
+    ResolveError(#[from] ResolveError),
+    NoNSEC,
+}
+
 impl <'r> NSECWalker<'r> {
     fn new(resolver: &'r Resolver, domain: &str) -> Result<Self,ProtoError> {
         let domain = domain.into_name()?;
         Ok(Self { resolver, current: domain.clone(), domain })
     }
-}
 
-impl <'r> Iterator for NSECWalker<'r> {
-    type Item = Name;
+    fn next_lookup(&mut self) -> Result<Option<Name>, NSecError> {
 
-    fn next(&mut self) -> Option<Self::Item> {
-        let next: Name = self.resolver
-            .lookup(self.current.clone(), NSEC)
-            .ok()?
-            .into_iter()
-            .next()?
-            .as_dnssec()?
-            .as_nsec()?
+        let rr = self.resolver
+        .lookup(self.current.clone(), NSEC)?
+        .into_iter()
+        .next()
+        .ok_or(NSecError::NoNSEC)?;
+
+        let next: Name = rr.as_dnssec()
+            .and_then(|ds| ds.as_nsec())
+            .ok_or(NSecError::NoNSEC)?
             .next_domain_name()
             .clone();
 
         // Parent domain in next name means end of zone
-        if next == self.domain { return None }
+        if next == self.domain { return Ok(None) }
 
         self.current = next.clone();
-        Some(next)
+        Ok(Some(next))      
+    }
+
+}
+
+impl <'r> Iterator for NSECWalker<'r> {
+    type Item = Result<Name, NSecError>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next_lookup().transpose()
     }
 }
 
@@ -52,12 +71,22 @@ fn main() -> Result<ExitCode, Box<dyn Error>> {
     let args = Args::parse();
     let domain = args.domain;
 
-    let resolver = Resolver::new(ResolverConfig::default(), ResolverOpts::default())?;
+    let mut config = ResolverConfig::default();
+
+    if let Some(ns) = args.nameserver {
+        let addr: SocketAddr = ns.parse()?;
+        let proto = if args.udp { Protocol::Udp } else {Protocol::Tcp };
+        config.add_name_server(NameServerConfig::new(addr, proto));
+    };
+
+    let resolver = Resolver::new(config, ResolverOpts::default())?;
+
     let walker = NSECWalker::new(&resolver, &domain)?;
 
     let mut found = false;
 
     for name in walker {
+        let name = name?;
         found = true;
         println!("{name}");
     }
